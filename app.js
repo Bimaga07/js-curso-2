36e35de07413ad67176c16250e3f7a8b0b140c79
Brigitte Bolaños <brigittebedo@hotmail.com>
2024-06-25 15:44:05 -0500

    alura latam inicio

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Hora de desafio";
+
+function aletFunction() {
+    alert("click")
+}
+function aletFunction() {
+    alert("yo amo JS")
+}
+function alertFunction() {
+    let ciudad = prompt("introduce el nombre de una ciudad de Brasil");
+    let mensaje = `estuve en ${ciudad} de brasil`;
+    alert(mensaje)
+}
+function suma() {
+    let numero1 = prompt("ingresa un numero");
+    numero1 = parseFloat(numero1);
+    let numero2 = prompt("ingresa el siguiente numero");
+    numero2 = parseFloat(numero2)
+    let suma = numero1 + numero2;
+    alert(`su suma es ${suma}`)
+   
+}
 
