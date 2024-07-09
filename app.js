89ca647e5e7e62cabbcdc516534d6af8badc57ac
Brigitte Bolaños <brigittebedo@hotmail.com>
2024-07-09 09:49:29 -0500

    basico

--- app.js
+++ app.js
@@ -22,3 +22,43 @@ function suma() {
    
 }
 
+function mostrar() {
+    console.log("hola mundo");
+}
+
+mostrar();
+
+function parametro(nombre){
+    console.log(`hola ${nombre}`);
+}
+parametro("brigitte");
+
+function doblarNumero(numero){
+    return numero * 2;
+
+}
+console.log(doblarNumero(5));
+
+function numeros(num1, num2, num3){
+    let suma = num1 + num2 + num3
+    let promedio = suma / 3
+    return promedio;
+}
+let promedio = numeros(5,6,8)
+console.log(promedio);
+
+function numeroMayor(num1, num2){
+    if(num1 > num2){
+        return num1
+    }else{
+        return num2
+    }
+
+}
+ console.log(numeroMayor(2,10));
+
+ function multiplicar(num1){
+    return num1 * num1
+
+ }
+ console.log(multiplicar(6));
